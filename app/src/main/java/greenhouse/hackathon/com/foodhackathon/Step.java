736d550ad25bb6d1a71885ae3014ece4a719ae1b
SanghwaKo <sanghwako@gmail.com>
2017-07-21 21:06:47 +0200

    -

--- app/src/main/java/greenhouse/hackathon/com/foodhackathon/Step.java
+++ app/src/main/java/greenhouse/hackathon/com/foodhackathon/Step.java
@@ -1,40 +1,13 @@
 package greenhouse.hackathon.com.foodhackathon;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 /**
  * Created by KSH on 2017-07-21.
  */
 
-public class Step implements Parcelable{
+public class Step{
     private String instruction;
     private String imgUrl;
 
-    public int describeContents(){
-        return  0;
-    }
-
-    public void writeToParcel(Parcel out, int flags){
-        out.writeString(instruction);
-        out.writeString(imgUrl);
-    }
-
-    public static final Parcelable.Creator<Step> CREATOR =
-            new Parcelable.Creator<Step>(){
-        public Step createFromParcel(Parcel in){
-            return new Step(in);
-        }
-
-        public Step[] newArray(int size){
-            return new Step[size];
-        }
-    };
-
-    private Step(Parcel in){
-        instruction = in.readString();
-        imgUrl = in.readString();
-    }
     public String getInstruction() {
         return instruction;
     }
